14200a96c46acde592ee3effc24bc82a4217026d
Jan Pobrislo <ccx@te2000.cz>
2025-02-11 02:54:48 +0000

    Replace parse_requirements function
    Instead of doing full parse of requirements and then stringifying them
    again simply do the minimal processing of requirements files to extract
    relevant parts of lines.

--- setup.py
+++ setup.py
@@ -4,22 +4,52 @@
 
 import pathlib
 
-import pkg_resources
 from setuptools import setup
 
 
-def parse_requirements(path: str) -> 'list[str]':
-    with pathlib.Path(path).open(encoding='utf-8') as requirements:
-        return [str(req) for req in pkg_resources.parse_requirements(requirements)]
+# tokenize requirements file
+# Note: according to the specification
+# at https://pip.pypa.io/en/stable/reference/requirements-file-format/
+# line continuations are processed before comments, however pkg_resources source code
+# says otherwise:
+# def parse_requirements(strs: _NestedStr) -> map[Requirement]:
+#     return map(Requirement, join_continuation(map(drop_comment, yield_lines(strs))))
+# Here we follow the actual implementation.
+def join_continuation(lines):
+    lines = iter(lines)
+    for item in lines:
+        while item.endswith('\\'):
+            try:
+                item = item[:-2].strip() + next(lines)
+            except StopIteration:
+                return
+        yield item
+
+
+def drop_comments(lines):
+    for item in lines:
+        yield item.partition(' #')[0]
+
+
+def drop_whites(lines):
+    for item in lines:
+        stripped = item.strip()
+        if stripped:
+            yield stripped
+
+
+def requirements_from_file(path: str) -> 'list[str]':
+    with pathlib.Path(path).open(encoding='utf-8') as f:
+        return list(drop_whites(join_continuation(drop_comments(f))))
 
 
 OPTIONAL_LINTERS = ['pylint', 'eradicate', 'radon', 'mypy', 'vulture']
 
 
 setup(
-    install_requires=parse_requirements('requirements/requirements.txt'),
+    install_requires=requirements_from_file('requirements/requirements.txt'),
     extras_require=dict(
-        tests=parse_requirements('requirements/requirements-tests.txt'),
+        tests=requirements_from_file('requirements/requirements-tests.txt'),
         all=OPTIONAL_LINTERS, **{linter: [linter] for linter in OPTIONAL_LINTERS},
         toml='tomli>=1.2.3; python_version < "3.11"',
     ),
