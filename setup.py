a96eacfcb1dd8902c57553362a94b0890ed0cb2c
Jan Pobrislo <ccx@te2000.cz>
2025-02-11 02:07:23 +0000

    Revert "Update setup.py"
    This reverts commit aea785349f1ab6f7739a55347af57fa680f3151d.

--- setup.py
+++ setup.py
@@ -4,32 +4,22 @@
 
 import pathlib
 
+import pkg_resources
 from setuptools import setup
+
+
+def parse_requirements(path: str) -> 'list[str]':
+    with pathlib.Path(path).open(encoding='utf-8') as requirements:
+        return [str(req) for req in pkg_resources.parse_requirements(requirements)]
+
 
 OPTIONAL_LINTERS = ['pylint', 'eradicate', 'radon', 'mypy', 'vulture']
 
 
 setup(
-    install_requires=[
-        'mccabe>=0.7.0',
-        'pycodestyle>=2.9.1',
-        'pydocstyle>=6.1.1',
-        'pyflakes>=2.5.0'
-    ],
+    install_requires=parse_requirements('requirements/requirements.txt'),
     extras_require=dict(
-tests=[
-            'pytest>=7.1.2',
-            'pytest-mypy',
-            'eradicate>=2.0.0',
-            'radon>=5.1.0',
-            'mypy',
-            'pylint>=2.11.1',
-            'pylama-quotes',
-            'toml',
-            'vulture',
-            'types-setuptools',
-            'types-toml'
-        ],
+        tests=parse_requirements('requirements/requirements-tests.txt'),
         all=OPTIONAL_LINTERS, **{linter: [linter] for linter in OPTIONAL_LINTERS},
         toml='tomli>=1.2.3; python_version < "3.11"',
     ),
